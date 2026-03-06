60d50a15d114bfa357f77e520b9a3636225220ae
re1sub <re1sub@proton.me>
2026-03-06 14:44:42 +0100

    add ports

--- app.js
+++ app.js
@@ -51,8 +51,8 @@ app.post("/api/test/email", async (req, res) => {
 
 	const transporter = nodemailer.createTransport({
 		host: process.env.MAIL_HOST,
-		port: 587,
-		secure: false,
+		port: Number(process.env.MAIL_PORT) || 587,
+		secure: Number(process.env.MAIL_PORT) === 465, // true for 465, false for 587
 		auth: {
 			user: process.env.MAIL_USER,
 			pass: process.env.MAIL_PASS,
